edb6cafc337affb761e59dcc1d23a98d7f9d57b9
Mikaela <gardemikaela@gmail.com>
2020-03-02 16:16:27 +0100

    nytt

--- movie-rating-predictor/movie-rating-predictor.py
+++ movie-rating-predictor/movie-rating-predictor.py
@@ -1,5 +1,9 @@
 # documentation to pandas: https://pandas.pydata.org/pandas-docs/stable/index.html
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import CategoricalNB, GaussianNB
+import numpy as np
 
 data1 = pd.read_csv('tmdb_5000_credits.csv',sep=',')
 data2 = pd.read_csv('tmdb_5000_movies.csv',sep=',')
@@ -26,10 +30,31 @@ for col_name in categorical.columns:
 numeric = df.select_dtypes(include=['float64', 'int64']).copy()
 col_num = []
 for col_name in numeric.columns:
-    col_num.append(col_name)
+    if col_name != 'vote_average':
+        col_num.append(col_name)
 
-#col_cat = ['cast', 'crew', 'genres', 'original_language']
-#print(df.info())
-#ML- algorithm Naive Bayes
+# Convension of data
+# Encode the categorical columns
+for col in col_cat:
+    l_e = LabelEncoder()
+    dt = l_e.fit_transform(df[col])
+    df[col] = dt
 
+vote_average = df['vote_average']
+df = df.drop(columns=['vote_average'])
+
+
+# ML- algorithm Naive Bayes
+X_train, X_test, y_train, y_test = train_test_split(df, vote_average, random_state=1, test_size=0.8)
+gaus_model = GaussianNB()
+h = np.array(X_train[col_num])
+e = np.array(y_train)
+gaus_model.fit(h, e)
+#gaus_y_pred = gaus_model.predict(X_test[col_num])
+#print(gaus_y_pred)
+
+#cat_model = CategoricalNB()
+#cat_model.fit(X_train[col_cat], y_train)
+#cat_y_pred = cat_model.predict(X_test[col_cat])
+#print(cat_y_pred)
 
